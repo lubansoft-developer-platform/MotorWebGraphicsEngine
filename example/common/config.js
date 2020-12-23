4b2e233544ca25518bad922418372f1e91657580
dulei <hengjun_du@sina.com>
2020-12-23 15:57:52 +0800

    更新官网工程

--- example/common/config.js
+++ example/common/config.js
@@ -3,7 +3,7 @@ var config = {
     motorAppId:"d0b3c61c6639434e84900b1fd8d391cb",
     motorSecret:"459dc8b77a63a0c009aec27f818febf6",
     bimProjectId:"bfdf508b-ae25-426a-ba6c-838f190eb4d6",
-    cimProjectId:"a0f833a0-7bc5-43ec-a360-f696d4a11b8d", //< 官网codebox工程
+    cimProjectId:"62453677-1903-4e24-a19e-776dc72ee21d", //< 官网codebox工程
     library:"../../library/motor.js"
 }
 
